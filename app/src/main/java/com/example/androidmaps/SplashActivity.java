76c3cdc3e37b39eac98432377f0207ed459ece3e
abhi7300 <abhishek1010mukherjee@gmail.com>
2016-07-03 21:09:59 +0530

    maps activity

--- app/src/main/java/com/example/androidmaps/SplashActivity.java
+++ app/src/main/java/com/example/androidmaps/SplashActivity.java
@@ -1,13 +1,30 @@
 package com.example.androidmaps;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start landing activity
+                Intent i = new Intent(SplashActivity.this, LandingActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
